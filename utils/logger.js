b260411ea7e85d5b84434e934f9ed6981327b6db
Andred17 <Damilaresammy1996@gmail.com>
2026-02-06 18:21:34 +0100

    Fix logger error with pino

--- utils/logger.js
+++ utils/logger.js
@@ -1,11 +1,21 @@
+// src/utils/logger.js
 import pino from "pino";
-import pretty from "pino-pretty";
 
-// Create a Pino logger instance
-const logger = pino(pretty({
-    colorize: true, // Enables colored logs in the terminal
-    translateTime: "HH:MM:ss Z", // Formats timestamps
-    ignore: "pid,hostname" // Removes unnecessary metadata
-}));
+const isProduction = process.env.NODE_ENV === "production";
+
+// Configure pino
+const logger = pino({
+  transport: !isProduction
+    ? {
+        target: "pino-pretty",
+        options: {
+          colorize: true,    
+          translateTime: "SYS:standard", 
+          ignore: "pid,hostname" 
+        }
+      }
+    : undefined, 
+  level: isProduction ? "info" : "debug" 
+});
 
 export default logger;
